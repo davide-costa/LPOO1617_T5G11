9d20f1cfbcb3d98db56dc22ee172d6e1f1829e85
DHFC <up201503995@fe.up.pt>
2017-02-21 16:51:19 +0000

    GameCreature implemented

--- Dungeon Keep/src/dkeep/logic/GameCreature.java
+++ Dungeon Keep/src/dkeep/logic/GameCreature.java
@@ -2,5 +2,44 @@ package dkeep.logic;
 
 public class GameCreature
 {
-
+	protected int x;
+	protected int y;
+	protected char symbol;
+	
+	public GameCreature(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int GetX() 
+	{
+		return x;
+	}
+	
+	public void SetX(int x) 
+	{
+		this.x = x;
+	}
+	
+	public int GetY() 
+	{
+		return y;
+	}
+	
+	public void SetY(int y)
+	{
+		this.y = y;
+	}
+	
+	public char GetSymbol()
+	{
+		return symbol;
+	}
+	
+	
+	public void SetSymbol(char symbol) 
+	{
+		this.symbol = symbol;
+	}
 }
