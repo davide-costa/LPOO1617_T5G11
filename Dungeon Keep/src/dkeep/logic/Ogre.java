05242dffdcb5a7ec5b1c7a728e8b84e96358ea2f
DHFC <davidehfc@gmail.com>
2017-02-21 21:33:48 +0000

    Added club coordinates

--- Dungeon Keep/src/dkeep/logic/Ogre.java
+++ Dungeon Keep/src/dkeep/logic/Ogre.java
@@ -2,6 +2,8 @@ package dkeep.logic;
 
 public class Ogre extends GameCreature
 {
+	private int club_x_pos = 5;
+	private int club_y_pos = 1;
 	private char club_symbol = '*';
 	public Ogre(int x, int y, char symbol)
 	{
