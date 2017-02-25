85256fe9a9b4b8587cdb6dcc6dd07dc800c976c0
DHFC <davidehfc@gmail.com>
2017-02-25 22:39:47 +0000

    Added functions GetClubSymbol() and SetClubSymbol()

--- Dungeon Keep/src/dkeep/logic/Ogre.java
+++ Dungeon Keep/src/dkeep/logic/Ogre.java
@@ -7,25 +7,39 @@ public class Ogre extends GameCreature
 	private int club_x = 5;
 	private int club_y = 1;
 	private char club_symbol = '*';
+	
 	public Ogre(int x, int y, char symbol)
 	{
 		super(x, y, 'O');
 	}
+	
 	public int GetClubX()
 	{
 		return club_x;
 	}
+	
 	public void SetClubX(int club_x)
 	{
 		this.club_x = club_x;
 	}
+	
 	public int GetClubY()
 	{
 		return club_y;
 	}
-	public void SetClubY(int club_y)
+	
+	public void SetClubY(char club_symbol)
 	{
 		this.club_y = club_y;
 	}
 	
+	public int GetClubSymbol()
+	{
+		return club_symbol;
+	}
+	
+	public void SetClubSymbol(char club_symbol)
+	{
+		this.club_symbol = club_symbol;
+	}
 }
