5b5723acb4a9517907382e01b8f824c5a191985e
Dinis <dinisvale@gmail.com>
2017-02-27 21:58:00 +0000

    Begun implementing multiple guard personalities.

--- Dungeon Keep/src/dkeep/logic/Guard.java
+++ Dungeon Keep/src/dkeep/logic/Guard.java
@@ -1,28 +1,17 @@
 package dkeep.logic;
 
-public class Guard extends GameCreature
+public abstract class Guard extends GameCreature
 {
-	private int movement_step = 0;
+	protected int movement_step = 0;
+	protected int x_path[] = {1};
+	protected int y_path[] = {2};
+	
 	
 	public Guard(int x, int y, char symbol)
 	{
 		super(x, y, 'G');
 	}
 	
-	public void Move()
-	{
-		if (movement_step == 24)
-			movement_step = 0;
-
-		if (movement_step == 0 || (movement_step >= 5 && movement_step < 11)) //for left moves
-			x--;
-		else if (movement_step >= 12 && movement_step <= 18) //for right moves
-			x++;
-		else if (movement_step >= 19 && movement_step <= 23) //for up moves
-			y--;
-		else if ((movement_step >= 1 && movement_step <= 4) || movement_step == 11) //for down moves
-			y++;
-
-		movement_step++;
-	}
+	public abstract void Move();
+	
 }
