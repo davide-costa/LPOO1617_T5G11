18c53fbb09ba1ea791c67ec7f4a0bce873f0de72
Dinis <dinisvale@gmail.com>
2017-02-28 00:58:56 +0000

    Fixed bug on guard's movement

--- Dungeon Keep/src/dkeep/logic/Guard.java
+++ Dungeon Keep/src/dkeep/logic/Guard.java
@@ -2,7 +2,7 @@ package dkeep.logic;
 
 public abstract class Guard extends GameCreature
 {
-	protected int movement_step = 0;
+	protected int movement_step = 1;
 	protected int x_path[] = {8,7,7,7,7,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,8,8,8,8};
 	protected int y_path[] = {1,1,2,3,4,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,5,4,3,2};
 	
