67a1e5c63fa85f7b91ec4f247e740b52f22904fa
Dinis Trigo <dinsivale@gmail.com>
2017-03-12 01:13:52 +0000

    Created a guard for the unit testing.

---
+++ Dungeon Keep/src/dkeep/test/TestGuard.java
@@ -0,0 +1,12 @@
+package dkeep.test;
+
+import dkeep.logic.GameCreature;
+
+public class TestGuard extends GameCreature
+{
+	public TestGuard(int x, int y, char symbol)
+	{
+		super(x, y, 'G');
+	}
+
+}
