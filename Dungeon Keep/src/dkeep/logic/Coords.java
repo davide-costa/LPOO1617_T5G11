0a9ddbfb1f1333262de287082061ef859f269613
Dinis Trigo <dinsivale@gmail.com>
2017-03-18 21:05:51 +0000

    Added class Coords to represent the coordinates on the game

---
+++ Dungeon Keep/src/dkeep/logic/Coords.java
@@ -0,0 +1,45 @@
+package dkeep.logic;
+
+public class Coords
+{
+	private int x;
+	private int y;
+	
+	public Coords(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int GetX()
+	{
+		return x;
+	}
+	
+	public void SetX(int x)
+	{
+		this.x = x;
+	}
+	
+	public int GetY()
+	{
+		return y;
+	}
+	
+	public void SetY(int y)
+	{
+		this.y = y;
+	}
+	
+	public void Set(Coords coords)
+	{
+		x = coords.x;
+		x = coords.y;
+	}
+	
+	public void Set(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+}
