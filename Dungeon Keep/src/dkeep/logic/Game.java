980cbf749082e706dd59dc1bf0562e60c70895e4
Dinis Trigo <up201504196@fe.up.pt>
2017-03-21 15:38:46 +0000

    Made some methods private on Game class

--- Dungeon Keep/src/dkeep/logic/Game.java
+++ Dungeon Keep/src/dkeep/logic/Game.java
@@ -314,7 +314,7 @@ public class Game implements Serializable
 		return 0;
 	}
 	
-	public void TryStunOgres() 
+	private void TryStunOgres() 
 	{
 		for(Ogre ogre: ogres)
 		{
@@ -323,7 +323,7 @@ public class Game implements Serializable
 		}
 	}
 
-	public boolean WasCaught()
+	private boolean WasCaught()
 	{
 		switch (level)
 		{
@@ -341,7 +341,7 @@ public class Game implements Serializable
 		return false;
 	}
 	
-	public boolean WasCaughtByMob(GameCreature mob)
+	private boolean WasCaughtByMob(GameCreature mob)
 	{
 		if (mob.GetSymbol() == 'g' || mob.GetSymbol() == '8')
 			return false;
@@ -349,7 +349,7 @@ public class Game implements Serializable
 		return CellsAreAdjacent(hero.GetCoords(), mob.GetCoords());
 	}
 	
-	public boolean WasCaughtByClub(Ogre ogre)
+	private boolean WasCaughtByClub(Ogre ogre)
 	{
 		return CellsAreAdjacent(hero.GetCoords(), ogre.GetClubCoords());
 	}
