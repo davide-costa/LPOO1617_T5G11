29fd333eb2957ef7c57cc90707fb25c1ec924bbf
DHFC <up201503995@fe.up.pt>
2017-03-21 16:51:06 +0000

    Continued development of iteration 5

--- Dungeon Keep/src/dkeep/gui/CustomizableJFrame.java
+++ Dungeon Keep/src/dkeep/gui/CustomizableJFrame.java
@@ -6,6 +6,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
 
 public class CustomizableJFrame extends JFrame
 {
@@ -40,11 +44,45 @@ public class CustomizableJFrame extends JFrame
 	public CustomizableJFrame() 
 	{
 		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 836, 512);
+		setBounds(100, 100, 1152, 851);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JButton btnOgre = new JButton("Ogre");
+		btnOgre.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				
+			}
+		});
+		btnOgre.setBounds(979, 238, 115, 29);
+		contentPane.add(btnOgre);
+		
+		JButton btnWall = new JButton("Wall");
+		btnWall.setBounds(979, 328, 115, 29);
+		contentPane.add(btnWall);
+		
+		JButton btnDoor = new JButton("Door");
+		btnDoor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnDoor.setBounds(979, 373, 115, 29);
+		contentPane.add(btnDoor);
+		
+		JButton btnHero = new JButton("Hero");
+		btnHero.setBounds(979, 283, 115, 29);
+		contentPane.add(btnHero);
+		
+		JButton btnKey = new JButton("Key");
+		btnKey.setBounds(979, 418, 115, 29);
+		contentPane.add(btnKey);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(32, 54, 900, 664);
+		contentPane.add(panel);
 	}
-
 }
