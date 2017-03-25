2cf6643b630273bf1c8fefe2d00732048f597a9c
Dinis Trigo <up201504196@fe.up.pt>
2017-03-25 01:12:13 +0000

    Separate Customization contructor's intructions in functions
    To improve "Write short units of code" guideline

--- Dungeon Keep/src/dkeep/gui/CustomizationJFrame.java
+++ Dungeon Keep/src/dkeep/gui/CustomizationJFrame.java
@@ -23,7 +23,15 @@ public class CustomizationJFrame extends JFrame
 	private JPanel contentPane;
 	private String element_selected = "";
 	private CustomizationArea cust_area;
-
+	private JButton btnOgre;
+	private JButton btnWall;
+	private JButton btnDoor;
+	private JButton btnHero;
+	private JButton btnKey;
+	private JButton btnSaveChanges;
+	private JButton btnExit;
+	
+	JLabel InfoLabel;
 	/**
 	 * Create the frame.
 	 */
@@ -36,17 +44,30 @@ public class CustomizationJFrame extends JFrame
 		contentPane.setLayout(null);
 		setVisible(true);
 		
-		//Create cust area
-		cust_area = new CustomizationArea(this);
-		cust_area.setBounds(38,51,846,637);
-		cust_area.GetCustKeepMap().CreateNewGameMap(x_size, y_size);
-		this.getContentPane().add(cust_area);
-		cust_area.requestFocusInWindow();
-		cust_area.repaint();
-		
-		   
-		
-		JButton btnOgre = new JButton("Ogre");
+		CreateCustArea(x_size, y_size);
+		CreateButtonOgre();
+		CreateButtonWall();
+		CreateButtonDoor();
+		CreateButtonHero();
+		CreateButtonKey();
+		CreateInfoLabel();
+		CreateButtonSaveChanges();
+		CreateButtonExit();		
+	}
+	
+	private void CreateCustArea(int x_size, int y_size)
+	{
+				cust_area = new CustomizationArea(this);
+				cust_area.setBounds(38,51,846,637);
+				cust_area.GetCustKeepMap().CreateNewGameMap(x_size, y_size);
+				this.getContentPane().add(cust_area);
+				cust_area.requestFocusInWindow();
+				cust_area.repaint();
+	}
+	
+	private void CreateButtonOgre()
+	{
+		btnOgre = new JButton("Ogre");
 		btnOgre.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent e) 
@@ -56,8 +77,11 @@ public class CustomizationJFrame extends JFrame
 		});
 		btnOgre.setBounds(979, 238, 115, 29);
 		contentPane.add(btnOgre);
-		
-		JButton btnWall = new JButton("Wall");
+	}
+	
+	private void CreateButtonWall()
+	{
+		btnWall = new JButton("Wall");
 		btnWall.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent e) 
@@ -67,8 +91,11 @@ public class CustomizationJFrame extends JFrame
 		});
 		btnWall.setBounds(979, 328, 115, 29);
 		contentPane.add(btnWall);
-		
-		JButton btnDoor = new JButton("Door");
+	}
+	
+	private void CreateButtonDoor()
+	{
+		btnDoor = new JButton("Door");
 		btnDoor.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent e) 
@@ -78,8 +105,11 @@ public class CustomizationJFrame extends JFrame
 		});
 		btnDoor.setBounds(979, 373, 115, 29);
 		contentPane.add(btnDoor);
-		
-		JButton btnHero = new JButton("Hero");
+	}
+	
+	private void CreateButtonHero()
+	{
+		btnHero = new JButton("Hero");
 		btnHero.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent e) 
@@ -89,8 +119,11 @@ public class CustomizationJFrame extends JFrame
 		});
 		btnHero.setBounds(979, 283, 115, 29);
 		contentPane.add(btnHero);
-		
-		JButton btnKey = new JButton("Key");
+	}
+	
+	private void CreateButtonKey()
+	{
+		btnKey = new JButton("Key");
 		btnKey.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent e) 
@@ -100,14 +133,20 @@ public class CustomizationJFrame extends JFrame
 		});
 		btnKey.setBounds(979, 418, 115, 29);
 		contentPane.add(btnKey);
-		
-		JLabel InfoLabel = new JLabel("<html>Keep map must contain:<br>1 to 5 ogres<br>1 hero<br>A closed map (walls closing map)<br>1 key or more</html>");
+	}
+	
+	private void CreateInfoLabel()
+	{
+		InfoLabel = new JLabel("<html>Keep map must contain:<br>1 to 5 ogres<br>1 hero<br>A closed map (walls closing map)<br>1 key or more</html>");
 		InfoLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		InfoLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		InfoLabel.setBounds(38, 717, 628, 95);
 		contentPane.add(InfoLabel);
-		
-		JButton btnSaveChanges = new JButton("Save Changes");
+	}
+	
+	private void CreateButtonSaveChanges()
+	{
+		btnSaveChanges = new JButton("Save Changes");
 		btnSaveChanges.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent e) 
@@ -138,9 +177,12 @@ public class CustomizationJFrame extends JFrame
 		});
 		btnSaveChanges.setBounds(979, 31, 115, 29);
 		contentPane.add(btnSaveChanges);
-		
+	}
+	
+	private void CreateButtonExit()
+	{
 		JFrame cust_frame = this;
-		JButton btnExit = new JButton("Exit");
+		btnExit = new JButton("Exit");
 		btnExit.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent e) 
@@ -150,7 +192,6 @@ public class CustomizationJFrame extends JFrame
 		});
 		btnExit.setBounds(979, 741, 115, 29);
 		contentPane.add(btnExit);
-		
 	}
 	
 	public String GetElementSelected()
