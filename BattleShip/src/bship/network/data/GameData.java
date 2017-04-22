ae3d0369f5b13d0df2257aa77427f7d5626a94fc
Dinis Trigo <up201504196@fe.up.pt>
2017-04-22 21:02:31 +0100

    Updated network data classes inheritance

--- BattleShip/src/bship/network/data/GameData.java
+++ BattleShip/src/bship/network/data/GameData.java
@@ -1,6 +1,10 @@
 package bship.network.data;
 
-public class GameData implements BattleShipData
+public class GameData extends BattleShipData
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2246624379612010428L;
 	public String stuff;
 }
