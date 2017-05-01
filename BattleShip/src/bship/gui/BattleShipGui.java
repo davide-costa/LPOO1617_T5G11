1a217f3360f09fada98e9bd9e5040faf8b712c6f
Davide Costa <davidehfc@gmail.com>
2017-05-01 19:04:08 +0100

    Added gui package and BattleShipGui class

---
+++ BattleShip/src/bship/gui/BattleShipGui.java
@@ -0,0 +1,58 @@
+package bship.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class BattleShipGui {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run() 
+			{
+				try 
+				{
+					BattleShipGui window = new BattleShipGui();
+					window.frame.setVisible(true);
+				} catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public BattleShipGui() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() 
+	{
+		CreateJFrame();
+
+	}
+
+	private void CreateJFrame() 
+	{
+		frame = new JFrame();
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		//frame.setUndecorated(true);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+	}
+}
