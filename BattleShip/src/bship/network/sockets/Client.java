21ed4af334cbffc28c06c5ec7d69cfd89e26a735
Dinis Trigo <up201504196@fe.up.pt>
2017-05-01 16:51:27 +0100

    Formated code on Client.java

--- BattleShip/src/bship/network/sockets/Client.java
+++ BattleShip/src/bship/network/sockets/Client.java
@@ -19,132 +19,134 @@ import bship.network.data.GameData;
 
 public class Client extends Observable implements Runnable {
 
-    /**
-     * Uses to connect to the server 
-     */
-    private Socket socket;
+	/**
+	* Uses to connect to the server
+	*/
+	private Socket socket;
 
-    /**
-     * For reading input from server. 
-     */
-    private ObjectInputStream socket_input;
+	/**
+	* For reading input from server.
+	*/
+	private ObjectInputStream socket_input;
 
-    /**
-     * For writing output to server. 
-     */
-    private ObjectOutputStream socket_output;
+	/**
+	* For writing output to server.
+	*/
+	private ObjectOutputStream socket_output;
 
-    /**
-     * Status of client. 
-     */
-    private boolean connected;
+	/**
+	* Status of client.
+	*/
+	private boolean connected;
 
-    /**
-     * Port number of server
-     */
-     private int port=5555; //default port
+	/**
+	* Port number of server
+	*/
+	private int port = 5555; //default port
 
-    /**
-     * Host Name or IP address in String form
-     */
-    private String hostName;
+							 /**
+							 * Host Name or IP address in String form
+							 */
+	private String hostName;
 
-    public Client(Opponent observer) throws IOException
-    {
+	public Client(Opponent observer) throws IOException
+	{
 		connected = false;
-        connect("dservers.ddns.net",5555);
-    }
+		connect("dservers.ddns.net", 5555);
+	}
 
-    public void connect(String hostName, int port) throws IOException {
-        if(!connected)
-        {
-	     this.hostName = hostName;
-           this.port = port;
-           socket = new Socket(hostName,port);
-           //get I/O from socket
-           this.socket_output = new ObjectOutputStream(socket.getOutputStream());
-           this.socket_input = new ObjectInputStream(socket.getInputStream());
-
-		   connected = true;
-           //initiate reading from server...
-           Thread t = new Thread(this);
-           t.start(); //will call run method of this class
-        }
-    }
-
-    public void sendBattleShipData(BattleShipData data) throws IOException
-    {
-		if(connected)
+	public void connect(String hostName, int port) throws IOException {
+		if (!connected)
 		{
-	        socket_output.writeObject(data);
-        } else throw new IOException("Not connected to server");
-    }
+			this.hostName = hostName;
+			this.port = port;
+			socket = new Socket(hostName, port);
+			//get I/O from socket
+			this.socket_output = new ObjectOutputStream(socket.getOutputStream());
+			this.socket_input = new ObjectInputStream(socket.getInputStream());
 
-    public void disconnect() {
-		if(socket != null && connected)
-        {
-          try {
-			socket.close();
-          }catch(IOException ioe) {
-			//unable to close, nothing to do...
-          }
-          finally {
-			this.connected = false;
-          }
-        }
-    }
+			connected = true;
+			//initiate reading from server...
+			Thread t = new Thread(this);
+			t.start(); //will call run method of this class
+		}
+	}
 
-    public void run() {
-	   BattleShipData data;
-         try {
-            while(connected && (data = (BattleShipData) socket_input.readObject())!= null)
-            {
-			 System.out.println(((GameData)data).stuff);
-			 //notify observers//
-			 this.setChanged();
- //notify+send out recieved msg to Observers
-              	 this.notifyObservers(data);
-            }
-         }
-         catch(IOException | ClassNotFoundException ioe) { }
-         finally { connected = false; }
-    }
+	public void sendBattleShipData(BattleShipData data) throws IOException
+	{
+		if (connected)
+		{
+			socket_output.writeObject(data);
+		}
+		else throw new IOException("Not connected to server");
+	}
 
-    public boolean isConnected() {
+	public void disconnect() {
+		if (socket != null && connected)
+		{
+			try {
+				socket.close();
+			}
+			catch (IOException ioe) {
+				//unable to close, nothing to do...
+			}
+			finally {
+				this.connected = false;
+			}
+		}
+	}
+
+	public void run() {
+		BattleShipData data;
+		try {
+			while (connected && (data = (BattleShipData)socket_input.readObject()) != null)
+			{
+				System.out.println(((GameData)data).stuff);
+				//notify observers//
+				this.setChanged();
+				//notify+send out recieved msg to Observers
+				this.notifyObservers(data);
+			}
+		}
+		catch (IOException | ClassNotFoundException ioe) {}
+		finally { connected = false; }
+	}
+
+	public boolean isConnected() {
 		return connected;
-    }
+	}
 
 
-    public int getPort(){
-            return port;
-        }
+	public int getPort() {
+		return port;
+	}
 
-    public void setPort(int port){
-            this.port = port;
-        }
+	public void setPort(int port) {
+		this.port = port;
+	}
 
-    public String getHostName(){
-            return hostName;
-        }
+	public String getHostName() {
+		return hostName;
+	}
 
-    public void setHostName(String hostName){
-            this.hostName = hostName;
-        }
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
 
 	//testing Client//
-    public static void main(String[] argv)throws IOException
-    {
-        String msg = "";
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        Client c = new Client(null);
-        while(!msg.equalsIgnoreCase("quit"))
-        {
-           msg = br.readLine();
-           BattleShipData data = new GameData();
-           ((GameData)data).stuff = msg;
-           c.sendBattleShipData(data);
-        }
-        c.disconnect();
-    }
+	public static void main(String[] argv)throws IOException
+	{
+		String msg = "";
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		Client c = new Client(null);
+		while (!msg.equalsIgnoreCase("quit"))
+		{
+			msg = br.readLine();
+			BattleShipData data = new GameData();
+			((GameData)data).stuff = msg;
+			c.sendBattleShipData(data);
+		}
+		c.disconnect();
+	}
 }
