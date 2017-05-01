2cb44947f7b57fed4aea03b75d03c9fcbd54fb0e
Dinis Trigo <up201504196@fe.up.pt>
2017-05-01 01:33:28 +0100

    Genertaed getters and setters for class CellState's attribs

--- BattleShip/src/bship/logic/CellState.java
+++ BattleShip/src/bship/logic/CellState.java
@@ -7,6 +7,26 @@ public class CellState
 	
 	public CellState(Ship ship)
 	{
+		this.setShip(ship);
+	}
+
+	public boolean isDiscovered()
+	{
+		return discovered;
+	}
+
+	public void setDiscovered(boolean discovered)
+	{
+		this.discovered = discovered;
+	}
+
+	public Ship getShip()
+	{
+		return ship;
+	}
+
+	public void setShip(Ship ship)
+	{
 		this.ship = ship;
 	}
 }
