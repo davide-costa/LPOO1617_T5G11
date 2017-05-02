69ad4c84948cd3f4d8dd38f5bfb4e4930a886160
DHFC <up201503995@fe.up.pt>
2017-05-02 16:18:50 +0100

    Added constructor and attributes to BattleShipServer

--- BattleShipServer/src/bship/logic/BattleShipServer.java
+++ BattleShipServer/src/bship/logic/BattleShipServer.java
@@ -1,6 +1,13 @@
 package bship.logic;
 
+import java.util.ArrayList;
+
 public class BattleShipServer
 {
-
+	ArrayList<Player> players;
+	
+	public BattleShipServer()
+	{
+		players = new ArrayList<Player>();
+	}
 }
