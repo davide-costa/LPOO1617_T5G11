49ecb794cd9af3c98839dd4ba55fa964839bc37a
Dinis Trigo <up201504196@fe.up.pt>
2017-05-04 01:33:27 +0100

    Changed Coords and CellState to Object.
    So that the server doesn't need to know the Coords and CellState classes

--- BattleShipServer/src/bship/network/data/GameResultData.java
+++ BattleShipServer/src/bship/network/data/GameResultData.java
@@ -2,29 +2,26 @@ package bship.network.data;
 
 import java.util.ArrayList;
 
-import bship.logic.CellState;
-import bship.logic.Coords;
-
 public class GameResultData extends GameData
 {
-	private ArrayList<Coords> coordsArray;
-	private ArrayList<CellState> resultStates;
+	private ArrayList<Object> coordsArray;
+	private ArrayList<Object> resultStates;
 	private boolean endOfGame;
 	
 	
-	public GameResultData(ArrayList<Coords> coordsArray, ArrayList<CellState> resultStates, boolean endOfGame)
+	public GameResultData(ArrayList<Object> coordsArray, ArrayList<Object> resultStates, boolean endOfGame)
 	{
 		this.coordsArray = coordsArray;
 		this.resultStates = resultStates;
 		this.endOfGame = endOfGame;
 	}
 	
-	public ArrayList<Coords> getCoordsArray()
+	public ArrayList<Object> getCoordsArray()
 	{
 		return coordsArray;
 	}
 	
-	public ArrayList<CellState> getResultStates()
+	public ArrayList<Object> getResultStates()
 	{
 		return resultStates;
 	}
