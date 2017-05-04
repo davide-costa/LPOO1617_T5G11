46f10871f81f7c4c9c7a17353cac127256b2c366
Dinis Trigo <up201504196@fe.up.pt>
2017-05-04 01:58:47 +0100

    Added initialization for battleshipPlayers ArrayList in Server class

--- BattleShipServer/src/bship/test/TestServerLogic.java
+++ BattleShipServer/src/bship/test/TestServerLogic.java
@@ -4,6 +4,15 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import bship.logic.BattleShipServer;
+
 public class TestServerLogic
 {
+	@Test
+	public void TestConstructor()
+	{
+		BattleShipServer server = new BattleShipServer();
+		assertEquals(server.getOnlinePlayers().size(), 0);
+		assertEquals(server.getBattleshipPlayers().size(), 0);
+	}
 }
