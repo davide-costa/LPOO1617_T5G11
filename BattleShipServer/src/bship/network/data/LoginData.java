3cdddd6a73d0522164b73f83a24921aa785a5209
Dinis Trigo <up201504196@fe.up.pt>
2017-05-04 02:30:18 +0100

    Made class LoginData not abstract

--- BattleShipServer/src/bship/network/data/LoginData.java
+++ BattleShipServer/src/bship/network/data/LoginData.java
@@ -2,7 +2,7 @@ package bship.network.data;
 
 import java.io.Serializable;
 
-public abstract class LoginData implements BattleShipData, Serializable
+public class LoginData implements BattleShipData, Serializable
 {
 	/**
 	 * 
