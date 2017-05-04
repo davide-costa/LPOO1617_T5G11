6530047d3fc9a581c12c38662b9aff68c8b20257
Dinis Trigo <up201504196@fe.up.pt>
2017-05-04 17:57:52 +0100

    Added tests for testing wrong login attempts

--- BattleShipServer/src/bship/test/TestServerLogic.java
+++ BattleShipServer/src/bship/test/TestServerLogic.java
@@ -24,7 +24,7 @@ public class TestServerLogic
 	}
 	
 	@Test
-	public void TestAccountCreation() throws UnknownHostException, IOException, InterruptedException, ClassNotFoundException
+	public void TestAccountCreationLogin() throws UnknownHostException, IOException, InterruptedException, ClassNotFoundException
 	{
 		BattleShipServer server = new BattleShipServer();
 		
@@ -45,7 +45,7 @@ public class TestServerLogic
 		BattleShipData data = new LoginRequestData("battleship", "lpoo");
 		socket_output.writeObject(data);
 		
-		Thread.sleep(2); //wait for the other thread to read information from the socket
+		Thread.sleep(200); //wait for the other thread to read information from the socket
 		assertEquals(1, server.getBattleshipPlayers().size());
 		assertEquals(1, server.getOnlinePlayers().size());
 
@@ -62,5 +62,54 @@ public class TestServerLogic
 		
 	}
 	
+	@Test
+	public void TestWrongPasswordLogin() throws UnknownHostException, IOException, InterruptedException, ClassNotFoundException
+	{
+		BattleShipServer server = new BattleShipServer();
+		
+		/**
+		* For reading input from server.
+		*/
+		ObjectInputStream socket_input;
+
+		/**
+		* For writing output to server.
+		*/
+		ObjectOutputStream socket_output;
+		
+		Socket socket = new Socket("127.0.0.1", 5555);
+		//get I/O from socket
+		socket_output = new ObjectOutputStream(socket.getOutputStream());
+		socket_input = new ObjectInputStream(socket.getInputStream());
+		
+		//Create account
+		BattleShipData data = new LoginRequestData("battleship", "lpoo");
+		socket_output.writeObject(data);
+		//Logout
+		socket.close();
+		
+		//Attempt to log in with wrong password
+		socket = new Socket("127.0.0.1", 5555);
+		socket_output = new ObjectOutputStream(socket.getOutputStream());
+		socket_input = new ObjectInputStream(socket.getInputStream());
+		data = new LoginRequestData("battleship", "wrongpwd");
+		socket_output.writeObject(data);
+		
+		Thread.sleep(2); //wait for the other thread to read information from the socket
+
+		LoginResponseData response = (LoginResponseData)socket_input.readObject();
+		//Ensure server responses with login failed
+		assertNotNull(response);
+		assertFalse(response.isSucceeded());
+		//Ensure the server does not add the player to the list of online players
+		assertEquals(1, server.getBattleshipPlayers().size());
+		assertEquals(0, server.getOnlinePlayers().size());
+		
+		//disconnect
+		socket.close();
+		Thread.sleep(200);
+		
+		
+	}
 	
 }
