6898860842687e570992d711b61a6444cb27244e
Dinis Trigo <up201504196@fe.up.pt>
2017-05-04 02:38:42 +0100

    Made LoginData abastract and created classes for request and response

--- BattleShipServer/src/bship/network/data/LoginData.java
+++ BattleShipServer/src/bship/network/data/LoginData.java
@@ -1,29 +1,6 @@
 package bship.network.data;
 
-import java.io.Serializable;
-
-public class LoginData implements BattleShipData, Serializable
+public abstract class LoginData implements BattleShipData
 {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -5676866486804632232L;
-	private String username;
-	private String password;
-	
-	public LoginData(String username, String password)
-	{
-		this.username = username;
-		this.password = password;
-	}
-	
-	public String getUsername() 
-	{
-		return username;
-	}
-	
-	public String getPassword()
-	{
-		return password;
-	}
+
 }
