396f794541f9b4ce7295742b4214226df26ebb9c
Dinis Trigo <up201504196@fe.up.pt>
2017-05-07 17:10:08 +0100

    Added test for Logging in with account previously created on server

--- BattleShipServer/src/bship/test/TestServerLogic.java
+++ BattleShipServer/src/bship/test/TestServerLogic.java
@@ -65,6 +65,59 @@ public class TestServerLogic
 
 		server.stopServer();
 	}
+
+	@Test
+	public void TestLoginWithAccountPreviouslyCreated() throws UnknownHostException, IOException, InterruptedException, ClassNotFoundException
+	{
+		BattleShipServer server = new BattleShipServer();
+		
+		/**
+		* For reading input from server.
+		*/
+		ObjectInputStream socket_input;
+
+		/**
+		* For writing output to server.
+		*/
+		ObjectOutputStream socket_output;
+		
+		Socket socket = new Socket("127.0.0.1", 5555);
+		//get I/O from socket
+		socket_output = new ObjectOutputStream(socket.getOutputStream());
+		socket_input = new ObjectInputStream(socket.getInputStream());
+		
+		//Create account
+		BattleShipData data = new LoginRequestData("battleship", "lpoo");
+		socket_output.writeObject(data);
+		//Logout
+		socket.close();
+		
+		Thread.sleep(200); //wait for server to close the socket on the other side
+		
+		//Attempt to log in with wrong password
+		socket = new Socket("127.0.0.1", 5555);
+		socket_output = new ObjectOutputStream(socket.getOutputStream());
+		socket_input = new ObjectInputStream(socket.getInputStream());
+		data = new LoginRequestData("battleship", "lpoo");
+		socket_output.writeObject(data);
+		
+		Thread.sleep(200); //wait for the other thread to read information from the socket
+
+		LoginResponseData response = (LoginResponseData)socket_input.readObject();
+		//Ensure server responses with login failed
+		assertNotNull(response);
+		assertTrue(response.isSucceeded());
+		//Ensure the server does not add the player to the list of online players
+		assertEquals(1, server.getBattleshipPlayers().size());
+		assertEquals(1, server.getOnlinePlayers().size());
+		
+		//disconnect
+		socket.close();
+		Thread.sleep(200);
+		
+
+		server.stopServer();
+	}
 	
 	@Test
 	public void TestWrongPasswordLogin() throws UnknownHostException, IOException, InterruptedException, ClassNotFoundException
@@ -118,5 +171,7 @@ public class TestServerLogic
 
 		server.stopServer();
 	}
+	
+	
 	
 }
