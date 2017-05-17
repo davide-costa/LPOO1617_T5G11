ffd96829d3208ca9625b75c7277f87c0be49f3b0
Dinis Trigo <up201504196@fe.up.pt>
2017-05-18 00:12:12 +0100

    Made constructors on Ship and Carrier classes

--- BattleShip/src/bship/logic/Carrier.java
+++ BattleShip/src/bship/logic/Carrier.java
@@ -2,8 +2,13 @@ package bship.logic;
 
 import java.util.ArrayList;
 
-public class Carrier extends Ship {
-
+public class Carrier extends Ship
+{
+	public Carrier()
+	{
+		super(5, "Carrier");
+	}
+	
 	public Carrier(ArrayList<Coords> coords,  String direction) 
 	{
 		super(5, coords, direction, "Carrier");
