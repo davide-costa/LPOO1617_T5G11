d3d84b2874a0313fd340cb8c5825b871705fd6be
Dinis Trigo <up201504196@fe.up.pt>
2017-05-22 03:47:48 +0100

    Added support for starting game when two players are InShipPlacement

--- BattleShipServer/src/bship/logic/InShipPlacement.java
+++ BattleShipServer/src/bship/logic/InShipPlacement.java
@@ -1,8 +1,11 @@
 package bship.logic;
 
+import java.io.IOException;
+
 import bship.network.data.BattleShipData;
 import bship.network.data.ReadyForGameData;
 import bship.network.data.ShipPlacementData;
+import bship.network.data.StartGameData;
 
 public class InShipPlacement extends PlayerState
 {
@@ -28,7 +31,36 @@ public class InShipPlacement extends PlayerState
 		if(data instanceof ReadyForGameData)
 		{
 			player.setState(new ReadyForGame(player));
-			player.getBattleShipServer().canGameBeInitiated(player, player.getOpponent());
+			Player opponent = player.getOpponent();
+			if (player.getBattleShipServer().canGameBeInitiated(player, opponent))
+			{
+				player.setState(new InGame(player));
+				opponent.setState(new InGame(opponent));
+				try
+				{
+					player.sendData(new StartGameData());
+					opponent.sendData(new StartGameData());
+				}
+				catch (IOException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			else
+			{
+				player.setState(new ReadyForGame(player));
+				try
+				{
+					opponent.sendData(data);
+				}
+				catch (IOException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 			//sets player state as readyforgame
 		}
 
