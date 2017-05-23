8d66df71ae3da5af49bd338c355e0271422ae759
Dinis Trigo <up201504196@fe.up.pt>
2017-05-23 16:00:38 +0100

    Updated LobbyInfoData

--- BattleShip/src/bship/network/data/LobbyInfoData.java
+++ BattleShip/src/bship/network/data/LobbyInfoData.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 
 public class LobbyInfoData extends LobbyData
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6318024591084146157L;
 	private ArrayList<String> onlinePlayersNames;
 	
 	public LobbyInfoData(ArrayList<String> onlinePlayersNames)
