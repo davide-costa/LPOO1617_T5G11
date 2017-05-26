16dd20e429ee1660086bf3f84f6ef896d087e52f
Dinis Trigo <up201504196@fe.up.pt>
2017-05-26 13:56:17 +0100

    Developed tests for update when received GameResultData

--- BattleShip/src/bship/test/TestMultiplayerOpponent.java
+++ BattleShip/src/bship/test/TestMultiplayerOpponent.java
@@ -10,6 +10,7 @@ import bship.logic.Coords;
 import bship.logic.Game;
 import bship.logic.MultiplayerOpponent;
 import bship.network.data.GameShootData;
+import bship.network.data.EndOfGameData;
 import bship.network.data.GameResultData;
 import bship.network.data.GameResultData.GameResult;
 import bship.network.sockets.Client;
@@ -75,30 +76,29 @@ public class TestMultiplayerOpponent
 		Coords shootCoords; 
 		GameResultData resultData;
 		GameResult result;
-		GameResultData sentData;
+		EndOfGameData sentData;
 		
 		shootCoords = new Coords(3, 5);
 		result = GameResult.HIT;
 		resultData = new GameResultData(result, false);
-		//game.setCurrResult(result);
 		game.setEndOfGame(false);
-		clientSocket.simulateReceptionOfData(shootData);
+		clientSocket.simulateReceptionOfData(resultData);
 		
-		assertEquals(shootCoords, game.getLastReceivedCoords());
-		sentData = (GameResultData) clientSocket.getLastBattleShipDataSent();
-		assertEquals(result, sentData.getResult());
-		assertEquals(false, sentData.isEndOfGame());
+		assertEquals(result, game.getCurrResult());
+		sentData = (EndOfGameData) clientSocket.getLastBattleShipDataSent();
+		assertNull(sentData);
+//		assertEquals(result, sentData.getResult());
+//		assertEquals(false, sentData.isEndOfGame());
 		
-		shootCoords = new Coords(1, 7);
-		shootData = new GameShootData(shootCoords);
-		result = GameResult.WATER;
-		game.setCurrResult(result);
+		shootCoords = new Coords(3, 5);
+		result = GameResult.HIT;
+		resultData = new GameResultData(result, true);
 		game.setEndOfGame(true);
-		clientSocket.simulateReceptionOfData(shootData);
+		clientSocket.simulateReceptionOfData(resultData);
 		
-		assertEquals(shootCoords, game.getLastReceivedCoords());
-		sentData = (GameResultData) clientSocket.getLastBattleShipDataSent();
-		assertEquals(result, sentData.getResult());
-		assertEquals(true, sentData.isEndOfGame());
+		assertEquals(result, game.getCurrResult());
+		sentData = (EndOfGameData) clientSocket.getLastBattleShipDataSent();
+		assertNotNull(sentData);
+		assertNotNull(sentData.getWinnerGameMap());
 	}
 }
