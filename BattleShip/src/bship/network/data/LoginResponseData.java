a1ef5369d8fe379ddc0eef83ab38199278085990
Davide Costa <davidehfc@gmail.com>
2017-05-27 23:32:01 +0100

    Added atribut to class LoginResponseData that indicates if a new Account was created

--- BattleShip/src/bship/network/data/LoginResponseData.java
+++ BattleShip/src/bship/network/data/LoginResponseData.java
@@ -7,13 +7,26 @@ public class LoginResponseData extends LoginData
 	 */
 	private static final long serialVersionUID = -2708819194232353251L;
 	boolean succeeded;
+	boolean newAccountCreated;
 	
 	public LoginResponseData(boolean succeeded)
 	{
 		this.succeeded = succeeded;
+		this.newAccountCreated = false;
 	}
+	
 	public boolean isSucceeded()
 	{
 		return succeeded;
+	}
+	
+	public void setAccountAsNew()
+	{
+		newAccountCreated = true;
+	}
+	
+	public boolean newAcoountCreated() 
+	{
+		return newAccountCreated;
 	}
 }
