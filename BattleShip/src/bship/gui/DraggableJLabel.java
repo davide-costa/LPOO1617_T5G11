e2c361fb01dc9ee5ff81e8cba69d99f1738b7d89
Dinis Trigo <up201504196@fe.up.pt>
2017-05-29 01:04:34 +0100

    Added class DraggableJLabel that is a JLabel draggable with the mouse

---
+++ BattleShip/src/bship/gui/DraggableJLabel.java
@@ -0,0 +1,84 @@
+package bship.gui;
+
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import javax.swing.JLabel;
+
+public class DraggableJLabel extends JLabel
+{
+	Point initClick;
+	
+	public DraggableJLabel(String text)
+	{
+		super(text);
+		this.addMouseMotionListener(new MouseMotionListener(){
+
+			@Override
+			public void mouseDragged(MouseEvent event)
+			{
+				Point mouseLocation = new Point(event.getX(), event.getY());
+				
+				if (initClick == null)
+				{
+					initClick = mouseLocation;
+					return;
+				}
+				Point labelLocation = DraggableJLabel.this.getLocation();
+				
+				//Determine how much the mouse moved since the initial click
+				int xMoved = (labelLocation.x + mouseLocation.x) - (labelLocation.x + initClick.x);
+				int yMoved = (labelLocation.y + mouseLocation.y) - (labelLocation.y + initClick.y);
+
+				//Move picture to this position
+				int newX = labelLocation.x + xMoved;
+				int newY = labelLocation.y + yMoved;
+				
+				DraggableJLabel.this.setLocation(newX, newY);
+				DraggableJLabel.this.repaint();
+			}
+
+			@Override
+			public void mouseMoved(MouseEvent event)
+			{
+
+			}
+		});
+		
+		this.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent event)
+			{
+				System.out.println("mouseClicked called");
+				initClick = event.getLocationOnScreen();
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent event)
+			{
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent event)
+			{
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent event)
+			{
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent event)
+			{
+				
+			}
+		});
+	}
+}
