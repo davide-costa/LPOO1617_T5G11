1a1ebd8df080d737ad9e634d0c196decf120163c
Davide Costa <davidehfc@gmail.com>
2017-06-11 17:57:21 +0100

    Improved AIOpponent legibility

--- BattleShip/src/bship/ai/AIOpponent.java
+++ BattleShip/src/bship/ai/AIOpponent.java
@@ -3,7 +3,6 @@ package bship.ai;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 import bship.logic.AllyCellState;
 import bship.logic.Coords;
@@ -11,33 +10,27 @@ import bship.logic.DefaultMap;
 import bship.logic.Game;
 import bship.logic.GameMap;
 import bship.logic.Ship;
-import bship.logic.ShipPlacement;
 import bship.logic.SinglePlayerShipPlacement;
 
 public class AIOpponent
 {
 	private Game game;
 	private GameMap map;
-	//private AIState state;
 	private HashMap<Integer, String> generatedDirection = new HashMap<Integer, String>();
 	private enum Direction {UP, DOWN, RIGHT, LEFT};
 	private ArrayList<Direction> mainDirections;
 	private ArrayList<Direction> secondaryDirections;
-	//private static int lastRandomHitCoords;
-//	private static boolean upChecked;
-//	private static boolean downChecked;
-//	private static boolean rightChecked;
-//	private static boolean leftChecked;
+
 	
 	public AIOpponent()
 	{
 		map = new DefaultMap(false);
 		mainDirections = new ArrayList<Direction>();
 		secondaryDirections = new ArrayList<Direction>();
+		fillDirections();
 	}
 	
-	//tem de ser chamado quando se passa a ai search...quando fazendo random se atinge um barco
-	private void FillDirections() 
+	private void fillDirections() 
 	{
 		mainDirections.add(Direction.UP);
 		mainDirections.add(Direction.DOWN);
@@ -47,12 +40,10 @@ public class AIOpponent
 		secondaryDirections.addAll(mainDirections);
 	}
 
-
 	private void initializeAllCellsToWater()
 	{
 		map.fill(new AllyCellState(null));
 	}
-
 
 	private void fillGeneratedDirection()
 	{
@@ -65,7 +56,7 @@ public class AIOpponent
 		return map;
 	}
 	
-	public void PerformShipPlacement(Game game, ArrayList<Ship> ships)
+	public void performShipPlacement(Game game, ArrayList<Ship> ships)
 	{
 		fillGeneratedDirection();
 		initializeAllCellsToWater();
@@ -104,40 +95,6 @@ public class AIOpponent
 	
 	public Coords generateAIShootCoords()
 	{
-		/*
-		switch(mainState)
-		{
-			case Horizontal:
-				switch(secondaryState)
-				{
-					case Left:
-						break;
-					case Right:
-						break;
-				}
-				
-			case Vertical:
-				switch(secondaryState)
-				{
-					case Up:
-						break;
-					case Down:
-						break;
-				}
-		}*/
-		Coords shootCoords = new Coords(0,0);
-
-		int maxXCoord = map.getMapXSize() - 1;
-		int maxYCoord = map.getMapYSize() - 1;
-		Random rand = new Random();
-		
-		do
-		{
-			shootCoords.SetX(rand.nextInt(maxXCoord));
-			shootCoords.SetY(rand.nextInt(maxYCoord));
-		}
-		while(!shootCoordsValid(shootCoords));
-		
-		return shootCoords;
+		return null;
 	}
 }
